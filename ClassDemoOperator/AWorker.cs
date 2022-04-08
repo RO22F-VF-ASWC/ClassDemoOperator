05c1ccc8a45507e8d05946a407b9771c15c2a42c
Peter Levinsky <pele@easj.dk>
2022-04-08 11:16:05 +0200

    Add project files.

---
+++ ClassDemoOperator/AWorker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ClassDemoOperator.model;
+
+namespace ClassDemoOperator
+{
+    internal class AWorker
+    {
+        public AWorker()
+        {
+        }
+
+        public void Start()
+        {
+            Position p1 = new Position(3, 4);
+            Position p2 = new Position(2, 6);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+
+
+
+        }
+    }
+}
---
+++ ClassDemoOperator/model/Position.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoOperator.model
+{
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position():this(0,0)
+        {
+        }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+        }
+
+        protected bool Equals(Position other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Position)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+
+
+        /*
+         * Operator overloads
+         */
+
+
+
+    }
+}
